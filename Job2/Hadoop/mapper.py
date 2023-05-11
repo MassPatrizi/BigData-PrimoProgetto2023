4c353cd1b931f557fad1d7070f9a8d8a8f51c8ec
Alessandro Sajeva <alessandro.sajeva@gmail.com>
2023-05-11 16:15:25 +0200

    iniziato job2 mapReduce

---
+++ Job2/Hadoop/mapper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""mapper.py"""
+import sys
+import datetime
+
+for row in sys.stdin:
+    row = row.strip().split(',')
+
+    # Ignora la riga di intestazione
+    if row[0] == 'Id':
+        continue
+
+    # Solo se row è di almeno 10 campi, printa userId, Numerator e Denominator
+    if len(row) >= 10:
+        userId = row[2]
+        helpfulnessNumerator = row[4]
+        helpfulnessDenominator = row[5]
+
+        print(f'{userId}\t{helpfulnessNumerator}\t{helpfulnessDenominator}')
