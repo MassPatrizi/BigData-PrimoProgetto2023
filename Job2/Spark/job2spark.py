52e655b2e77be097fe8d6212c7fdd836fcc621d9
Alessandro Sajeva <alessandro.sajeva@gmail.com>
2023-05-19 22:57:09 +0200

    job2spark working but needs refactoring

--- Job2/Spark/job2spark.py
+++ Job2/Spark/job2spark.py
@@ -29,18 +29,23 @@ input_RDD = spark.sparkContext.textFile(input_filepath).cache()
 header = input_RDD.first()
 input_RDD = input_RDD.filter(f=lambda line: line != header)
 
-# Run transformations asking them directly to the RDDs
+# Mapping phase
 # create a RDD splitting the csv by ";"
 reviews_RDD = input_RDD.map(f=lambda line: line.strip().split(";"))
 # create an RDD mapping only uderID, Num e Den
 user_num_den_RDD = reviews_RDD.map(f=lambda line: (line[2], line[4], line[5]))
 # create and RDD mapping userID, utility
 user_utility_RDD = user_num_den_RDD.map(f=lambda line: (line[0], float_division(float(line[1]),float(line[2]))))
-# Run the final action / actions asking them directly to the RDDs
-
-
+# Compute the mean for each key
+# solution is here
+# https://stackoverflow.com/questions/29930110/calculating-the-averages-for-each-key-in-a-pairwise-k-v-rdd-in-spark-with-pyth
+# first lambda is aggregation within-partition, second lambda is cross-partition
+aTuple = (0,0)
+aggregated_by_key_RDD = user_utility_RDD.aggregateByKey(aTuple, lambda a,b: (a[0] + b, a[1] + 1), lambda a,b: (a[0] + b[0], a[1] + b[1]))
+finalResult = aggregated_by_key_RDD.mapValues(lambda v: v[0]/v[1])
+sorted_RDD = finalResult.sortBy(lambda x: -x[1])
 # Write down the output
-user_utility_RDD.saveAsTextFile(output_filepath)
+sorted_RDD.saveAsTextFile(output_filepath)
 
 # Chiudi la sessione Spark
 spark.stop()
