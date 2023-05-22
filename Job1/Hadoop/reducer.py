707398cd3ba40097d66b4151a337e20bb834e804
Massimiliano Patrizi <40141221+MassPatrizi@users.noreply.github.com>
2023-05-22 10:59:02 +0200

    Job1 (Hive) in local & cluster

--- Job1/Hadoop/reducer.py
+++ Job1/Hadoop/reducer.py
@@ -8,7 +8,7 @@ import time
 
 reducer_start_time = time.time()
 
-# Defining the number of top products and top words per product
+# Definisci il numero di Prodotti e parole per prodotto per ogni anno
 TOP_PRODUCTS = 10
 TOP_WORDS_PER_PRODUCT = 5
 
@@ -16,8 +16,8 @@ current_year = None
 current_product_counts = {}
 current_product_reviews = {}
 
+# Funzione per calcolare le top words
 def emit_top_words(product_id, words, year, reviews):
-    # Emitting the top words for a given product
     top_words = heapq.nlargest(TOP_WORDS_PER_PRODUCT, words, key=lambda k: words[k])
     for word in top_words:
         cleaned_word = re.sub(r'[^a-zA-Z]', '', word)  # Applica la regex per rimuovere caratteri non alfabetici
@@ -26,34 +26,32 @@ def emit_top_words(product_id, words, year, reviews):
             output_data.append((year, product_id, cleaned_word, count, reviews))
 
 
-output_data = []  # List to store the (year, product, word, count, reviews) tuples
+output_data = []
 
 for line in sys.stdin:
-    # Parsing the input line
     values = line.strip().split('\t')
 
-    # Verifying that the line contains all the expected values
+    # Verifica se la linea in input contiene i campi aspettati
     if len(values) != 3:
         print(f"Illegible input: {line}", file=sys.stderr)
         continue
 
     ProductId, year, Text = values
 
-    # If a new year is encountered
+    # Se incontra un nuovo anno
     if current_year != year:
-        # If there was a year being processed, emit the result
         if current_year is not None:
             for ProductId in current_product_counts:
                 product_counts = current_product_counts[ProductId]
                 product_reviews = current_product_reviews[ProductId]
                 emit_top_words(ProductId, product_counts, current_year, product_reviews)
 
-        # Reset the variables for the new year
+        # Resetta le variabili per l'anno nuovo
         current_year = year
         current_product_counts = {}
         current_product_reviews = {}
 
-    # Update the counts for the current product
+    # Aggiorno il conteggio per il prodotto corrente
     if ProductId in current_product_counts:
         product_counts = current_product_counts[ProductId]
         for word in Text.split():
@@ -63,7 +61,7 @@ for line in sys.stdin:
         current_product_counts[ProductId] = {}
         current_product_reviews[ProductId] = 0
 
-    # Update the (word, count) tuple for the current product
+    # Aggiorgna la coppia (word, count) per il prodotto corrente
     current_product_reviews[ProductId] += 1
     product_counts = current_product_counts[ProductId]
     for word in Text.split():
@@ -71,22 +69,22 @@ for line in sys.stdin:
             product_counts[word] = product_counts.get(word, 0) + 1
 
 
-# Emitting the result for the last year
+# Calcola il risultato finale per l'anno corrente
 if current_year is not None:
     for ProductId in current_product_counts:
         product_counts = current_product_counts[ProductId]
         product_reviews = current_product_reviews[ProductId]
         emit_top_words(ProductId, product_counts, current_year, product_reviews)
 
-# Sorting the output_data by year and reviews in descending order
-output_data_sorted = sorted(output_data, key=lambda x: (x[0], x[4]), reverse=True)
+# Ordina l'output per anno e numero di recensioni
+output_data_sorted = sorted(output_data, key=lambda x: (x[0], -x[4]))
 
-# Print the final output
+# Fase di output
 previous_year = None
 rows_per_year = {}
-max_rows_per_year = 50
-max_rows_per_ProductId = 5
-max_products_per_year = 10
+max_rows_per_year = 50      # Massimo numero di righe in output per ogni anno
+max_rows_per_ProductId = 5  # Massimo numero di righe per uno stesso ProductId
+max_products_per_year = 10  # Massimo numero di ProductId per uno stesso anno
 products_per_year = 0
 
 for year, ProductId, word, count, reviews in output_data_sorted:
---
+++ Job1/Hive/Job1Cluster.hql
@@ -0,0 +1,105 @@
+-- Creazione della tabella reviews
+DROP TABLE reviews;
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id STRING,
+    productid STRING,
+    userid STRING,
+    profilename STRING,
+    helpfulnessnumerator INT,
+    helpfulnessdenominator INT,
+    score INT,
+    `time` INT,
+    summary STRING,
+    text STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ';'
+STORED AS TEXTFILE
+TBLPROPERTIES("skip.header.line.count"="1");
+
+-- Caricamento dei dati nella tabella reviews
+LOAD DATA LOCAL INPATH 'input/ReviewsCleaned.csv' INTO TABLE reviews;
+--LOAD DATA LOCAL INPATH 'input/duplicated_output.csv' INTO TABLE reviews;
+--LOAD DATA LOCAL INPATH 'input/quintupled_output.csv' INTO TABLE reviews;
+--LOAD DATA LOCAL INPATH 'input/decupled_output.csv' INTO TABLE reviews;
+
+-- Generazione dei risultati per ciascun anno
+-- reviews_by_year seleziona tutte le ennuple prendendo year, productid e text
+WITH reviews_by_year AS (
+    SELECT
+        year(FROM_UNIXTIME(`time`)) AS year,
+        productid,
+        text
+    FROM
+        reviews
+),
+-- top_products conta tutte le righe con uno stesso year e productid
+top_products AS (
+    SELECT
+        year,
+        productid,
+        COUNT(*) AS review_count    --quante volte appaiono insieme un year e productid
+    FROM
+        reviews_by_year
+    GROUP BY
+        year,
+        productid
+),
+top_10_products AS (
+    SELECT
+        year,
+        productid,
+        review_count,
+        ROW_NUMBER() OVER (PARTITION BY year ORDER BY review_count DESC) AS rank
+    FROM
+        top_products
+),
+tw AS (
+    SELECT
+        year,
+        productid,
+        regexp_replace(word, '[^a-zA-Z0-9\s]', '') AS cleaned_word,
+        word_count,
+        ROW_NUMBER() OVER (PARTITION BY year, productid ORDER BY word_count DESC) AS rank
+    FROM (
+        SELECT
+            year,
+            productid,
+            regexp_replace(word, '[^a-zA-Z0-9\s]', '') AS word,
+            COUNT(*) AS word_count
+        FROM
+            reviews_by_year
+        LATERAL VIEW EXPLODE(SPLIT(text, ' ')) word_table AS word
+        WHERE
+            LENGTH(regexp_replace(word, '[^a-zA-Z0-9\s]', '')) >= 4
+        GROUP BY
+            year,
+            productid,
+            word
+    ) word_counts
+)
+SELECT
+    'year' AS year,
+    'productid' AS productid,
+    'review_count' AS review_count,
+    'word' AS word,
+    'word_count' AS word_count
+UNION ALL
+SELECT
+    CAST(tp.year AS STRING) AS year,
+    tp.productid,
+    CAST(tp.review_count AS STRING) AS review_count,
+    tw.cleaned_word AS word,
+    CAST(tw.word_count AS STRING) AS word_count
+FROM
+    top_10_products tp
+JOIN 
+    tw ON tp.year = tw.year AND tp.productid = tw.productid AND tw.rank <= 5
+WHERE
+    tp.rank <= 10
+ORDER BY
+    year ASC,
+    review_count DESC,
+    productid,
+    word_count DESC;
